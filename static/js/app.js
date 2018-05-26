0fe343f5ec2ab251fc0b493030af75a606fe0a9c
John Ewan <j-ewan@northwestern.edu>
2018-05-26 14:20:02 -0500

    updates to app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,63 +1,26 @@
-build_DD()
-
 function build_DD() {
-    var dd_stuff = Plotly.d3.select("selDataset")
+    //var dd_stuff = Plotly.d3.select("#selDataset")
+    var dd_stuff = document.getElementById("selDataset")
     var dd_url = "/names";
     Plotly.d3.json(dd_url, function (error, response) {
         // if error
         // loop & append
+        console.log(dd_stuff)
         for (var i = 0; i < response.length; i++){
             // create option tag
+            var elements = document.createElement("option");
             // get the element
+            var arr = response[i];
             // add text
+            elements.textContent = arr;
             // add value to option tag
+            elements.value = arr
             // append to dropdown
-            dd_stuff.appe 
-            for(var i = 0; i< response.length;i++){
-                var elements = document.createElement("option");
-                var arr = response[i];
-                elements.textContent = arr;
-                elements.value = arr;
-                dropdown.appendChild(elements);
+            console.log(elements)
+            console.log(dd_stuff)
+            dd_stuff.appendChild(elements);
+            
         };
-        }
+    })};
 
-    })
-}
-
-
-
-function buildPlot() {
-    /* data route */
-    var url = "/api/pals";
-    Plotly.d3.json(url, function (error, response) {
-
-        console.log(response);
-
-        var data = response
-
-        var layout = {
-            scope: "usa",
-            title: "Pet Pals",
-            showlegend: false,
-            height: 600,
-            // width: 980,
-            geo: {
-                scope: "usa",
-                projection: {
-                    type: "albers usa"
-                },
-                showland: true,
-                landcolor: "rgb(217, 217, 217)",
-                subunitwidth: 1,
-                countrywidth: 1,
-                subunitcolor: "rgb(255,255,255)",
-                countrycolor: "rgb(255,255,255)"
-            },
-        };
-
-        Plotly.newPlot("plot", data, layout);
-    });
-}
-
-buildPlot();
+    build_DD()
